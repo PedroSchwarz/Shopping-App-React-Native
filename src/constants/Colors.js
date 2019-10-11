cfdc1edce4026ee44feb7c67579e6de69127506f
PedroSchwarz <pedroschawarz97@gmail.com>
2019-10-11 13:40:51 -0300

    Colors constant object added

---
+++ src/constants/Colors.js
@@ -0,0 +1,12 @@
+export default {
+  primary: "#ff9800",
+  darkPrimary: "#f57c00",
+  accent: "#7c4dff",
+  success: "#4caf50",
+  error: "#f44336",
+  warning: "#ffc107",
+  light: "#fff",
+  dark: "#424242",
+  grey: "#f2f2f2",
+  lightText: "#9c9c9c"
+};
